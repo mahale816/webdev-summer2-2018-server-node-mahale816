ed9d4d243ab1df6f610933cf56a897984c8b25bd
mahale816 <mahale.c@husky.neu.edu>
2018-08-09 21:22:47 -0400

    heroku changes

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require('express')
 var bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URL);
-// mongoose.connect('mongodb://localhost/webdev-summer2-2018');
+mongoose.connect('mongodb://heroku_tn9mhkkw:b1e68ffpula27d8c7gepuepj5v@ds119052.mlab.com:19052/heroku_tn9mhkkw');
 
 var app = express()
 
